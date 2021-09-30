fdf94294c29b91c883d8226d957b2ebd1f8dfe3f
Philip T <69321457+philipscoderepo@users.noreply.github.com>
2021-09-30 08:37:07 -0700

    Add files via upload

---
+++ EmployeePractice/EmployeePractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First Integer:");
+            string input = Console.ReadLine();
+            int firstNum = Int32.Parse(input);
+
+            Console.WriteLine("Enter the Second Integer:");
+            input = Console.ReadLine();
+            int secondNum = Int32.Parse(input);
+
+            Console.WriteLine($"The sum of {firstNum} + {secondNum} = {firstNum + secondNum}");
+        }
+    }
+}
