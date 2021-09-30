2b658c96de9481374bd28f83f7861aff330fbaf4
Philip T <69321457+philipscoderepo@users.noreply.github.com>
2021-09-30 08:36:16 -0700

    Add files via upload

---
+++ Counting/Counting/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Counting
+{
+    class Program
+    {
+        static string MakeALine(int num)
+        {
+            string input = $"{num} ";
+            System.Text.StringBuilder sb = new System.Text.StringBuilder(50);
+            //iterate a number of times equal to the value of num
+            for (int i = 0; i < num; i++)
+            {
+                //this builds each individual character in the string
+                sb.Append(input);
+            }
+            return sb.ToString();
+        }
+
+        static string MakeAllLines(int num)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder(500);
+            //each iteration of the loop calls MakeALine using i to count up
+            for(int i = 0; i < num; i++)
+            {
+                sb.Append(MakeALine(i + 1));
+                sb.Append('\n');
+            }
+
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage: this program expects to be provided a single integer");
+                return;
+            }
+            int number;
+            if(int.TryParse(args[0], out number))
+            {
+                Console.WriteLine(MakeAllLines(number));
+            }
+            else
+            {
+                Console.WriteLine($"Usage: the supplied parameter '{args[0]}' was not a valid integer");
+            }
+
+        }
+    }
+}
---
+++ EmployeeInfo/EmployeeInfo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EmployeeInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lucinda Potter",
+            dob = "06/24/1992",
+            workPhoneNumber = "312-982-1010",
+            email = "ewoz@woz-u.com";
+
+
+            Console.WriteLine($"Name: {name}\n" +
+                $"Date of Birth: {dob}\n" +
+                $"Extension: {workPhoneNumber}\n" +
+                $"Email: {email}\n");
+        }
+    }
+}
