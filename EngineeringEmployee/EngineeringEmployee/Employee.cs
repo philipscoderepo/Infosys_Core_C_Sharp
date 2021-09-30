2033e9d23e4fd6383b6fdea3b5207ca47e63a0e3
Philip T <69321457+philipscoderepo@users.noreply.github.com>
2021-09-30 08:38:22 -0700

    Add files via upload

---
+++ EngineeringEmployee/EngineeringEmployee/Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace EngineeringEmployee
+{
+	class Employee
+	{
+		string _name;
+		int _salary;
+		string _hireDate;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		public int Salary
+		{
+			get { return _salary; }
+			set { _salary = value; }
+		}
+
+		public string HireDate
+		{
+			get { return _hireDate; }
+			set { _hireDate = value; }
+		}
+
+
+		public Employee(string name, int salary, string hireDate)
+		{
+			Name = name;
+			Salary = salary;
+			HireDate = hireDate;
+		}
+
+		public virtual void GetName()
+		{
+			Console.WriteLine($"Employee Name: {Name}");
+		}
+
+		public virtual void GetSalary()
+		{
+			Console.WriteLine($"Salary: ${Salary}");
+		}
+
+		public virtual void GetHireDate()
+		{
+			Console.WriteLine($"Hire Date: {HireDate}");
+		}
+
+	}
+
+	class Engineer : Employee
+	{
+		string _schoolAttended;
+
+		public string SchoolAttended
+		{
+			get { return _schoolAttended; }
+			set { _schoolAttended = value; }
+		}
+
+		public Engineer(string name, int salary, string hireDate, string schoolAttended) : base(name, salary, hireDate)
+		{
+			SchoolAttended = schoolAttended;
+		}
+	}
+
+	class SoftwareEngineer : Engineer
+	{
+
+		public SoftwareEngineer(string name, int salary, string hireDate, string schoolAttended) : base(name, salary, hireDate, schoolAttended) { }
+
+		public override void GetSalary()
+		{
+			Console.WriteLine("Salary: Sorry this information is private");
+		}
+
+	}
+}
---
+++ EngineeringEmployee/EngineeringEmployee/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EngineeringEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engineer e = new Engineer("Rupert Scott", 42000, "11/22/12", "Sacramento University");
+            e.GetName();
+            e.GetSalary();
+            e.GetHireDate();
+
+            Console.WriteLine(String.Empty.PadLeft(50, '-'));
+
+            SoftwareEngineer se = new SoftwareEngineer("Shea Rovington", 42000, "03/27/18", "Harvard University");
+            se.GetName();
+            se.GetSalary();
+            se.GetHireDate();
+        }
+    }
+}
