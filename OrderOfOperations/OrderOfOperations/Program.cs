87067b6b5fcf4de6d1ad368274f9f5c1705a9e4e
Philip T <69321457+philipscoderepo@users.noreply.github.com>
2021-09-30 08:40:33 -0700

    Add files via upload

---
+++ OrderOfOperations/OrderOfOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OrderOfOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int r = 10;
+
+            Console.WriteLine(Math.PI);
+            Console.WriteLine(2 * Math.PI);
+            Console.WriteLine($"A Circle with radius 10 jas a circumference of using Math.PI: {2 * Math.PI * r}");
+            Console.WriteLine($"A Circle with radius 10 jas a circumference of using 6.28318...: {6.283185307179586 * r}");
+            Console.WriteLine($"A Circle with radius 10 jas a circumference of using 355/133: {2 * (355/113) * r}");
+            //This last computation is incorrect because the result is cast as an integer. This is because the datatypes of the numbers
+            //in the calculation are in integer form. To fix this you could make 2 to 2.0.
+        }
+    }
+}
---
+++ ProcessInvoices/ProcessInvoices/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace ProcessInvoices
+{
+    class Program
+    {
+
+        static decimal ComputeLateFees(DateTime currentDate, DateTime dueDate, decimal amountDue)
+        {
+            int daysLate = (currentDate - dueDate).Days;
+            //if the days late is less than 1, the invoice is not late
+            if(daysLate < 1)
+            {
+                return 0;
+            }
+            //10% for the first 7 days
+            if(daysLate < 8)
+            {
+                return amountDue * .1M * daysLate;
+            }
+            //10% for the first 7 days and then 1% for every day after that
+            return (amountDue * .1M * 7) + (amountDue * .01M * (daysLate - 7));
+        }
+
+        static void Main(string[] args)
+        {
+            DateTime date;
+            //Checks to see if a date was provided as an arg
+            if(args.Length == 0)
+            {
+                date = DateTime.Now;
+                Console.WriteLine("No date provided, using current date");
+            }
+            //Checks to see if the date supplied was on one line of input
+            else if(args.Length != 1)
+            {
+                Console.WriteLine("Argument invalid: only supply one line of input");
+                return;
+            }
+            //Checks to see if the date is correctly formatted
+            else if(!DateTime.TryParse(args[0], out date))
+            {
+                Console.WriteLine($"{args[0]} was not understood as a valid date");
+                return;
+            }
+
+            Console.WriteLine($"Calculating fees based on {date.ToShortDateString()}:\n");
+            var reader = File.OpenText("invoices.dat");
+            string line;
+            while((line = reader.ReadLine()) != null)
+            {
+                string[] data = line.Split(",");
+                if(data.Length != 3)
+                {
+                    Console.WriteLine("Invalid data format");
+                    continue;
+                }
+
+                int invoiceID;
+                DateTime invoiceDate;
+                decimal invoiceAmount;
+
+                //Check to see if the data is valid
+                if(!int.TryParse(data[0], out invoiceID))
+                {
+                    Console.WriteLine($"{data[0]} is an invalid invoice ID");
+                    continue;
+                }
+                if (!DateTime.TryParse(data[1], out invoiceDate))
+                {
+                    Console.WriteLine($"{data[1]} is an invalid date");
+                    continue;
+                }
+                if (!decimal.TryParse(data[2], out invoiceAmount))
+                {
+                    Console.WriteLine($"{data[2]} is an invalid invoice amount");
+                    continue;
+                }
+
+                //Output the invoices
+                Console.WriteLine($"Invoice ID: {invoiceID} " +
+                    $"\n************************************************************************************\n" +
+                    $"Due Date: {invoiceDate.ToShortDateString()}" +
+                    $"   Invoice Amount: {invoiceAmount}" +
+                    $"   Late Fees: {ComputeLateFees(date, invoiceDate, invoiceAmount):0.00}\n"); 
+
+            }
+
+            reader.Close();
+        }
+    }
+}
